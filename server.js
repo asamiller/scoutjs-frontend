894ec5c318df4b801a4fac3273c7cacce2860fc5
Asa Miller <asa@standin.io>
2015-08-25 15:22:03 -0700

    update to new webpack build

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
+var path = require('path');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
+  contentBase: path.join(__dirname, './dist'),
   publicPath: config.output.publicPath,
-  hot: true,
   historyApiFallback: true,
+  hot: true,
   stats: {
     colors: true
   }
 }).listen(3000, 'localhost', function (err) {
-  if (err) {
-    console.log(err);
-  }
-
+  if (err) console.log(err);
   console.log('Listening at localhost:3000');
 });
